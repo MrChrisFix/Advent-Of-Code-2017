e6b1b7ee40afec271fce622b1f5fdbcd500b1706
Christian <christian.fikus@wp.pl>
2024-11-20 00:58:22 +0100

    Day 6

--- Advent-Of-Code-2017/Days/Day6.cpp
+++ Advent-Of-Code-2017/Days/Day6.cpp
@@ -1,13 +1,107 @@
 #include "../general.h"
+#include <set>
 
-string Day6_Part1(stringstream& input)
+int Day6_Part1(stringstream& input)
 {
-	return "Not implemented yet";
+	std::vector<int> banks;
+
+	while (!input.eof())
+	{
+		int num;
+		input >> num;
+		banks.push_back(num);
+	}
+
+	std::set< std::vector<int>> apperences;
+	int steps = 1;
+
+	while (true)
+	{
+		auto it = std::max_element(banks.begin(), banks.end());
+
+		int index = std::distance(banks.begin(), it);
+		int value = *it;
+		banks[index] = 0;
+
+		while (value > 0)
+		{
+			index = (index + 1) % banks.size();
+
+			banks[index]++;
+			value--;
+		}
+		if (apperences.contains(banks))
+			break;
+
+		apperences.insert(banks);
+		steps++;
+	}
+	
+	return steps;
 }
 
-string Day6_Part2(stringstream& input)
+int Day6_Part2(stringstream& input)
 {
-	return "Not implemented yet";
+	std::vector<int> banks;
+
+	while (!input.eof())
+	{
+		int num;
+		input >> num;
+		banks.push_back(num);
+	}
+
+	std::set< std::vector<int>> apperences;
+	int steps = 1;
+
+	std::vector<int> special;
+
+	while (true)
+	{
+		auto it = std::max_element(banks.begin(), banks.end());
+
+		int index = std::distance(banks.begin(), it);
+		int value = *it;
+		banks[index] = 0;
+
+		while (value > 0)
+		{
+			index = (index + 1) % banks.size();
+
+			banks[index]++;
+			value--;
+		}
+		if (apperences.contains(banks))
+		{
+			special = banks;
+			break;
+		}
+
+		apperences.insert(banks);
+	}
+	while (true)
+	{
+		auto it = std::max_element(banks.begin(), banks.end());
+
+		int index = std::distance(banks.begin(), it);
+		int value = *it;
+		banks[index] = 0;
+
+		while (value > 0)
+		{
+			index = (index + 1) % banks.size();
+
+			banks[index]++;
+			value--;
+		}
+		if (banks == special)
+		{
+			break;
+		}
+		steps++;
+	}
+
+	return steps;
 }
 
 
