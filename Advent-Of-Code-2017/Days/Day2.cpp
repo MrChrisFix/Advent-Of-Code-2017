ed021bd5037789704ef6923f1bf88f9903a1c9a7
Christian <christian.fikus@wp.pl>
2024-11-19 22:47:15 +0100

    Day 2

--- Advent-Of-Code-2017/Days/Day2.cpp
+++ Advent-Of-Code-2017/Days/Day2.cpp
@@ -1,13 +1,66 @@
 #include "../general.h"
 
-string Day2_Part1(stringstream& input)
+int Day2_Part1(stringstream& input)
 {
-	return "Not implemented yet";
+	int sum = 0;
+	while (!input.eof())
+	{
+		std::string sline;
+		getline(input, sline);
+		stringstream line(sline);
+
+		int min = -1, max = -1;
+		while(!line.eof())
+		{
+			int num;
+			line >> num;
+			if (min == -1)
+				min = max = num;
+			
+			if (min > num)
+				min = num;
+
+			if (max < num)
+				max = num;
+		}
+		sum += max - min;
+	}
+	return sum;
 }
 
-string Day2_Part2(stringstream& input)
+int Day2_Part2(stringstream& input)
 {
-	return "Not implemented yet";
+	int sum = 0;
+	while (!input.eof())
+	{
+		std::string sline;
+		getline(input, sline);
+		stringstream line(sline);
+
+		std::vector<int> nums;
+		while (!line.eof())
+		{
+			int num;
+			line >> num;
+			nums.push_back(num);
+		}
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			for (int j = i + 1; j < nums.size(); j++)
+			{
+				if (nums[i]% nums[j] == 0)
+				{
+					sum += nums[i] / nums[j];
+				}
+				else if (nums[j] % nums[i] == 0)
+				{
+					sum += nums[j] / nums[i];
+				}
+			}
+		}
+	}
+	return sum;
 }
 
 
