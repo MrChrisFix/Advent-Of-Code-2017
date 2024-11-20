0c4ae6ea902f2096a9c3689af0def17daace2ac4
Christian <christian.fikus@wp.pl>
2024-11-20 20:50:51 +0100

    Day 13

--- Advent-Of-Code-2017/Days/Day13.cpp
+++ Advent-Of-Code-2017/Days/Day13.cpp
@@ -1,13 +1,80 @@
 #include "../general.h"
+#include <map>
 
-string Day13_Part1(stringstream& input)
+int scannerPos(int picosecond, int range)
 {
-	return "Not implemented yet";
+	return picosecond % ((range-1)*2);
 }
 
-string Day13_Part2(stringstream& input)
+
+int Day13_Part1(stringstream& input)
 {
-	return "Not implemented yet";
+	int picosecond = 0;
+	int severity = 0;
+	while (!input.eof())
+	{
+		int layer, range;
+		char trash;
+		input >> layer >> trash >> range;
+
+		while (layer != picosecond)
+			picosecond++;
+
+		int scanner = scannerPos(picosecond, range);
+
+		if (scanner == 0)
+		{
+			severity += layer * range;
+		}
+
+		picosecond++;
+	}
+
+	return severity;
+}
+
+int Day13_Part2(stringstream& input)
+{
+	std::map<int, int> ranges;
+
+	
+	while (!input.eof())
+	{
+		int layer, range;
+		char trash;
+		input >> layer >> trash >> range;
+
+		ranges[layer] = range;
+	}
+
+	int delay = 0;
+	for (;; delay++)
+	{
+		bool fail = false;
+		int picosecond = delay;
+		int severity = 0;
+		int layer = 0;
+		for (auto& el : ranges)
+		{
+			while (el.first != layer)
+			{
+				picosecond++;
+				layer++;
+			}
+
+			if (scannerPos(picosecond, el.second) == 0)
+			{
+				fail = true;
+				break; 
+			}
+			picosecond++;
+			layer++;
+		}
+		if (!fail)
+			break;
+	}
+
+	return delay;
 }
 
 
