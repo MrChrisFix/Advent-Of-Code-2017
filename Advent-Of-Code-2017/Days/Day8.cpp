820ce4672a1dae4242e4c953d5c506aae408fcae
Christian Fikus <christian.fikus@wp.pl>
2024-11-20 15:10:02 +0100

    Day 8

--- Advent-Of-Code-2017/Days/Day8.cpp
+++ Advent-Of-Code-2017/Days/Day8.cpp
@@ -1,13 +1,82 @@
 #include "../general.h"
+#include <map>
 
-string Day8_Part1(stringstream& input)
+
+int Day8_Part1(stringstream& input)
 {
-	return "Not implemented yet";
+	std::map<std::string, int> registers;
+
+	while (!input.eof())
+	{
+		std:string sline;
+		getline(input, sline);
+		std::stringstream line(sline);
+		
+		std::string reg, if_reg, instr, cond;
+		int val, if_val;
+
+		line >> reg >> instr >> val >> cond >> if_reg >> cond >> if_val;
+
+
+
+		if (cond == "<" && registers[if_reg] < if_val ||
+			cond == "<=" && registers[if_reg] <= if_val ||
+			cond == ">" && registers[if_reg] > if_val ||
+			cond == ">=" && registers[if_reg] >= if_val ||
+			cond == "==" && registers[if_reg] == if_val ||
+			cond == "!=" && registers[if_reg] != if_val
+			)
+		{
+			registers[reg] += (instr == "inc" ? val : -val);
+		}
+
+	}
+
+	int max = (*registers.begin()).second;
+
+	for (auto& reg : registers)
+	{
+		if (reg.second > max)
+			max = reg.second;
+	}
+
+	return max;
 }
 
-string Day8_Part2(stringstream& input)
+int Day8_Part2(stringstream& input)
 {
-	return "Not implemented yet";
+	std::map<std::string, int> registers;
+
+	int highest = 0;
+
+	while (!input.eof())
+	{
+	std:string sline;
+		getline(input, sline);
+		std::stringstream line(sline);
+
+		std::string reg, if_reg, instr, cond;
+		int val, if_val;
+
+		line >> reg >> instr >> val >> cond >> if_reg >> cond >> if_val;
+
+		if (cond == "<" && registers[if_reg] < if_val ||
+			cond == "<=" && registers[if_reg] <= if_val ||
+			cond == ">" && registers[if_reg] > if_val ||
+			cond == ">=" && registers[if_reg] >= if_val ||
+			cond == "==" && registers[if_reg] == if_val ||
+			cond == "!=" && registers[if_reg] != if_val
+			)
+		{
+			registers[reg] += (instr == "inc" ? val : -val);
+
+			if (registers[reg] > highest)
+				highest = registers[reg];
+		}
+
+	}
+
+	return highest;
 }
 
 
