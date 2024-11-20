3da5ad4bc477f1d770b3f89c2f0605bc3ef9dd9b
Christian Fikus <christian.fikus@wp.pl>
2024-11-20 15:33:07 +0100

    Day 9

--- Advent-Of-Code-2017/Days/Day9.cpp
+++ Advent-Of-Code-2017/Days/Day9.cpp
@@ -1,13 +1,73 @@
 #include "../general.h"
 
-string Day9_Part1(stringstream& input)
+int Day9_Part1(stringstream& input)
 {
-	return "Not implemented yet";
+	std::string stream = input.str();
+	int score = 0;
+
+	int nestLevel = 0;
+	bool garbage = false;
+
+	for (size_t i = 0; i < stream.size(); i++)
+	{
+		char newChar = stream[i];
+		if (newChar == '!')
+		{
+			i++;
+			continue;
+		}
+		else if (newChar == '{' && !garbage)
+		{
+			nestLevel++;
+		}
+		else if (newChar == '}' && !garbage)
+		{
+			score += nestLevel;
+			nestLevel--;
+		}
+		else if (newChar == '<' && !garbage)
+		{
+			garbage = true;
+		}
+		else if (newChar == '>')
+		{
+			garbage = false;
+		}
+	}
+
+	return score;
 }
 
-string Day9_Part2(stringstream& input)
+int Day9_Part2(stringstream& input)
 {
-	return "Not implemented yet";
+	std::string stream = input.str();
+
+	bool garbage = false;
+	int garbageCnt = 0;
+
+	for (size_t i = 0; i < stream.size(); i++)
+	{
+		char newChar = stream[i];
+		if (newChar == '!')
+		{
+			i++;
+			continue;
+		}
+		else if (newChar == '<' && !garbage)
+		{
+			garbage = true;
+		}
+		else if (newChar == '>')
+		{
+			garbage = false;
+		}
+		else if(garbage)
+		{
+			garbageCnt++;
+		}
+	}
+
+	return garbageCnt;
 }
 
 
