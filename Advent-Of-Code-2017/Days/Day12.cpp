c37094f7e22413fb4c4b077018d5c909a9d9cd88
Christian <christian.fikus@wp.pl>
2024-11-20 20:12:43 +0100

    Day 12

--- Advent-Of-Code-2017/Days/Day12.cpp
+++ Advent-Of-Code-2017/Days/Day12.cpp
@@ -1,13 +1,87 @@
 #include "../general.h"
+#include <map>
+#include <set>
 
-string Day12_Part1(stringstream& input)
+void visit(int current, std::map<int, std::vector<int>>& connections, std::set<int>& visited)
 {
-	return "Not implemented yet";
+	if (visited.contains(current))
+		return;
+
+	visited.insert(current);
+	for (auto& el : connections[current])
+	{
+		visit(el, connections, visited);
+	}
 }
 
-string Day12_Part2(stringstream& input)
+int Day12_Part1(stringstream& input)
 {
-	return "Not implemented yet";
+	std::map<int, std::vector<int>> connections;
+	std::set<int> visited;
+
+	while (!input.eof())
+	{
+		std::string sline;
+		getline(input, sline);
+		std::stringstream line(sline);
+
+		int num1, numElse;
+		std::string trash;
+
+		line >> num1 >> trash >> numElse >> trash;
+
+		connections[num1].push_back(numElse);
+
+		while (!line.eof())
+		{
+			line >> numElse >> trash;
+			connections[num1].push_back(numElse);
+		}
+	}
+
+	visit(0, connections, visited);
+
+
+	return visited.size();
+}
+
+int Day12_Part2(stringstream& input)
+{
+	std::map<int, std::vector<int>> connections;
+	std::set<int> visited;
+
+	while (!input.eof())
+	{
+		std::string sline;
+		getline(input, sline);
+		std::stringstream line(sline);
+
+		int num1, numElse;
+		std::string trash;
+
+		line >> num1 >> trash >> numElse >> trash;
+
+		connections[num1].push_back(numElse);
+
+		while (!line.eof())
+		{
+			line >> numElse >> trash;
+			connections[num1].push_back(numElse);
+		}
+	}
+
+	int groups = 0;
+
+	for (auto& el : connections)
+	{
+		if (visited.contains(el.first))
+			continue;
+
+		visit(el.first, connections, visited);
+		groups++;
+	}
+
+	return groups;
 }
 
 
