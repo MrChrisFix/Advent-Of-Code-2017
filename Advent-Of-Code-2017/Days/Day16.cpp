0524dbc41a3eeeaee8371dc65f50ceebb045e43c
Christian <christian.fikus@wp.pl>
2024-11-20 23:09:52 +0100

    Dya 16

--- Advent-Of-Code-2017/Days/Day16.cpp
+++ Advent-Of-Code-2017/Days/Day16.cpp
@@ -1,21 +1,112 @@
 #include "../general.h"
+#include <numeric>
 
 string Day16_Part1(stringstream& input)
 {
-	return "Not implemented yet";
+	std::string programs;
+	programs.resize(16);
+	std::iota(programs.begin(), programs.end(), 'a');
+
+	while (!input.eof())
+	{
+		char c = input.get();
+
+		if (c == 's') //spin
+		{
+			int X;
+			input >> X;
+
+			std::rotate(programs.rbegin(), programs.rbegin() + X, programs.rend());
+
+		}
+		else if (c == 'x') //exchange
+		{
+			int A, B;
+			input >> A >> c >> B;
+
+			char tmp = programs[A];
+			programs[A] = programs[B];
+			programs[B] = tmp;
+
+		}
+		else //partner
+		{
+			char A, B;
+			input >> A >> c >> B;
+
+			auto Apos = programs.find(A);
+			auto Bpos = programs.find(B);
+
+			char tmp = programs[Apos];
+			programs[Apos] = programs[Bpos];
+			programs[Bpos] = tmp;
+		}
+
+		input >> c; //comma
+	}
+	return programs;
 }
 
 string Day16_Part2(stringstream& input)
 {
-	return "Not implemented yet";
+	std::string programs;
+	programs.resize(16);
+	std::iota(programs.begin(), programs.end(), 'a');
+
+	for (size_t i = 0; i < 1000000000 % 30; i++)
+	{
+		std::string sline = input.str();
+		input.seekg(0);
+		stringstream once(sline);
+
+		while (!once.eof())
+		{
+			char c = once.get();
+
+			if (c == 's') //spin
+			{
+				int X;
+				once >> X;
+
+				std::rotate(programs.rbegin(), programs.rbegin() + X, programs.rend());
+
+			}
+			else if (c == 'x') //exchange
+			{
+				int A, B;
+				once >> A >> c >> B;
+
+				char tmp = programs[A];
+				programs[A] = programs[B];
+				programs[B] = tmp;
+
+			}
+			else //partner
+			{
+				char A, B;
+				once >> A >> c >> B;
+
+				auto Apos = programs.find(A);
+				auto Bpos = programs.find(B);
+
+				char tmp = programs[Apos];
+				programs[Apos] = programs[Bpos];
+				programs[Bpos] = tmp;
+			}
+
+			once >> c; //comma
+		}
+	}
+
+	return programs;
 }
 
 
 void Day16(stringstream& input)
 {
-	cout<<"Part 1 result is: " << Day16_Part1(input)<<endl;
+	cout << "Part 1 result is: " << Day16_Part1(input) << endl;
 	input.seekg(0);
-	cout<<"Part 2 result is: " << Day16_Part2(input)<<endl<<endl;
+	cout << "Part 2 result is: " << Day16_Part2(input) << endl << endl;
 
 	return;
 }
